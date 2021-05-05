b456b01e25bd57f26a8d431b4b3e2b1225af92e2
Paal Kobbeltvedt <paal.kobbeltvedt@goodtech.no>
2021-05-05 08:20:26 +0200

    Created more 'tests'

--- src/test/java/no/logging/slf4j/logging/Slf4jLoggerTest.java
+++ src/test/java/no/logging/slf4j/logging/Slf4jLoggerTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
 @SpringBootTest
 class Slf4jLoggerTest {
 
@@ -11,10 +12,43 @@ class Slf4jLoggerTest {
     Slf4jLogger logger;
 
     @Test
-    public void test(){
+    public void verifyLoggerName(){
         logger.setLogger(Slf4jLoggerTest.class);
         Assert.assertTrue(logger.getName() == Slf4jLoggerTest.class.getName());
         logger.info(logger.getName());
     }
+
+    @Test
+    public void trace(){
+        logger.setLogger(Slf4jLoggerTest.class);
+        logger.trace("Dette {} en {} test", "er", "en");
+    }
+
+
+    @Test
+    public void info(){
+        logger.setLogger(Slf4jLoggerTest.class);
+        logger.info("Dette {} en {} test", "er", "en");
+    }
+
+    @Test
+    public void debug(){
+        logger.setLogger(Slf4jLoggerTest.class);
+        logger.debug("Dette {} en {} test", "er", "en");
+    }
+
+    @Test
+    public void warn(){
+        logger.setLogger(Slf4jLoggerTest.class);
+        logger.warn("Dette {} en {} test", "er", "en");
+    }
+
+    @Test
+    public void error(){
+        logger.setLogger(Slf4jLoggerTest.class);
+        logger.error("Dette {} en {} test", "er", "en");
+    }
+
+
 
 }
